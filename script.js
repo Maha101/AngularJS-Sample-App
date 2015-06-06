1c79ae35bf549df17253ae17dfd4df70b00f9035
ksrinivaspura <ksrinivaspura@riverbed.com>
2015-06-06 10:01:56 +0530

    passing arguments to pages

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
 			})
 
 			// route for the about page
-			.when('/about', {
+			.when('/about/:userName?', {
 				templateUrl : 'pages/about.html',
 				controller  : 'aboutController'
 			})
@@ -24,16 +24,18 @@
 			});
 	});
 
+
 	// create the controller and inject Angular's $scope
 	scotchApp.controller('mainController', function($scope) {
 		// create a message to display in our view
 		$scope.message = 'Everyone come and see how good I look!';
 	});
 
-	scotchApp.controller('aboutController', function($scope) {
-		$scope.message = 'Look! I am an about page.';
+	scotchApp.controller('aboutController', function($scope, $routeParams) {
+    $scope.message = 'Look! I am an about page. ' + $routeParams.userName;
+        //$scope.userName = "kiran" + $routeParams.userName;
 	});
 
 	scotchApp.controller('contactController', function($scope) {
-		$scope.message = 'Contact us! JK. This is just a demo.';
+        $scope.message = 'Contact us! JK. This is just a demo.';        
 	});
