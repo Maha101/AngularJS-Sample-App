3fd004b8d12e535e90029a1117222e9e6824c45d
ksrinivaspura <ksrinivaspura@riverbed.com>
2015-06-07 10:10:47 +0530

    Basic Layout

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
-	// create the module and name it scotchApp
-	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+// create the module and name it InventoryApp
+var InventoryApp = angular.module('InventoryApp', ['ngRoute']);
 
-	// configure our routes
-	scotchApp.config(function($routeProvider) {
-		$routeProvider
+// configure our routes
+InventoryApp.config(function($routeProvider) {
+	$routeProvider
 
-			// route for the home page
-			.when('/', {
-				templateUrl : 'pages/home.html',
-				controller  : 'mainController'
-			})
+		// route for the home page
+		.when('/', {
+			templateUrl : 'pages/home.html',
+			controller  : 'mainController'
+		})
 
-			// route for the about page
-			.when('/about/:userName?', {
-				templateUrl : 'pages/about.html',
-				controller  : 'aboutController'
-			})
+		// route for the about page
+		.when('/about/:userName?', {
+			templateUrl : 'pages/about.html',
+			controller  : 'aboutController'
+		})
 
-			// route for the contact page
-			.when('/contact', {
-				templateUrl : 'pages/contact.html',
-				controller  : 'contactController'
-			});
-	});
+		// route for the contact page
+		.when('/contact', {
+			templateUrl : 'pages/contact.html',
+			controller  : 'contactController'
+		})
+
+		// route for the contact page
+		.when('/itemlist', {
+            templateUrl : 'pages/itemlist.html',
+            controller  : 'itemListController'
+        });
+});
 
 
-	// create the controller and inject Angular's $scope
-	scotchApp.controller('mainController', function($scope) {
-		// create a message to display in our view
-		$scope.message = 'Everyone come and see how good I look!';
-	});
+// create the controller and inject Angular's $scope
+InventoryApp.controller('mainController', function($scope) {
+	// create a message to display in our view
+	$scope.message = 'Everyone come and see how good I look!';
+});
 
-	scotchApp.controller('aboutController', function($scope, $routeParams) {
+InventoryApp.controller('aboutController', function($scope, $routeParams) {
     $scope.message = 'Look! I am an about page. ' + $routeParams.userName;
-        //$scope.userName = "kiran" + $routeParams.userName;
-	});
+    //$scope.userName = "kiran" + $routeParams.userName;
+});
 
-	scotchApp.controller('contactController', function($scope) {
-        $scope.message = 'Contact us! JK. This is just a demo.';        
-	});
+InventoryApp.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';        
+});
+
+InventoryApp.controller('itemListController', function ($scope) {
+    $scope.message = 'Item List';
+});
