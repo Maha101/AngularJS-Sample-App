ba14a44ccbffad206382a068ac0c3f9d6e758a2e
ksrinivaspura <ksrinivaspura@riverbed.com>
2015-06-16 10:30:45 +0530

    Implemented code for to send email for daily reports

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
     http = require('http').Server(app),
     bodyParser = require('body-parser');
         
-var db = require('./src/db');
+var db = require('./src/db'), 
+    mail = require('./src/send_mail'),
+    moment = require('moment');
 
 var debug = require('debug'),
     events = require('events'),
@@ -229,9 +231,79 @@ app.post('/api/sell_stock', function (req, res) {
     eventEmitter.emit("next_item");
 });
 
+function sendDailyReport() {
+    var count = 0;
+    var current_stocks, incoming_stocks, outgoing_stocks;
+    eventEmitter.on('done', function () {
+        count++;
+        if(3 !== count) {
+            INFO("Got partial Data");
+            return;
+        }
+
+        INFO("Got all data. NOw send email");
+        eventEmitter.removeAllListeners('done');
+        var html_text = "<html><head><title>Daily Report</title></head><body>";
+        html_text = "<table border='1'><tr><th>Current Stocks</th><th>Incoming Stocks </th> <th> Outgoing Stocks</th></tr><td>";
+        var i = 0;
+        html_text += "<table border='1'><thead><tr><th>Item Name</th><th>Quantity</th></tr></thead><tbody>";        
+        for(i = 0; i < current_stocks.length; i++) {
+           html_text += "<tr>"; 
+           html_text += "<td>" + current_stocks[i].name + "</td>"; 
+           html_text += "<td>" + (current_stocks[i].quantity/1000).toFixed(3)+ "</td>"; 
+           html_text += "</tr>"; 
+        }
+        html_text += "</tbody></table></td><td>";
+
+        html_text += "<table border='1'><thead><tr><th>Item Name</th><th>Quantity</th><th>Price</th></tr></thead><tbody>";
+        for(i = 0; i < incoming_stocks.length; i++) {
+           html_text += "<tr>"; 
+           html_text += "<td>" + incoming_stocks[i].name + "</td>"; 
+           html_text += "<td>" + (incoming_stocks[i].quantity/1000).toFixed(3)+ "</td>"; 
+           html_text += "<td>" + incoming_stocks[i].price+ "</td>"; 
+           html_text += "</tr>"; 
+        }
+        html_text += "</tbody></table></td><td>";
+
+        html_text += "<table border='1'><thead><tr><th>Item Name</th><th>Quantity</th><th>Price</th></tr></thead><tbody>";
+        for(i = 0; i < outgoing_stocks.length; i++) {
+           html_text += "<tr>"; 
+           html_text += "<td>" + outgoing_stocks[i].name + "</td>"; 
+           html_text += "<td>" + (outgoing_stocks[i].quantity/1000).toFixed(3)+ "</td>"; 
+           html_text += "<td>" + outgoing_stocks[i].price+ "</td>"; 
+           html_text += "</tr>"; 
+        }
+        html_text += "</tbody></table></td>";
+
+        html_text += "</table></body></html>";
+        INFO(html_text);
+        mail.SendMail('kiranstalin@gmail.com', 'Daily Report for ' + moment().format('DD-MM-YYYY'), html_text, function(error) {
+            INFO("Send Mail status %s", error);                
+        });
+    });
+
+    db.current_stocks(function(err, obj) {
+        current_stocks = obj;
+        eventEmitter.emit("done");
+    });
+
+    var obj = {};
+    obj.to = moment().format('YYYY-MM-DD');
+    obj.from = moment().format('YYYY-MM-DD');
+    db.get_stock_details(obj, function (error, rows) {
+        incoming_stocks = rows;
+        eventEmitter.emit("done");
+    });
+
+    db.get_sales_details(obj, function (error, rows) {
+        outgoing_stocks = rows;
+        eventEmitter.emit("done");
+    });
+}
 
 server = http.listen(port, function(){
     console.log("listening on http://%s:%d", hostname, port);
+    sendDailyReport();
 });
 
 process.on('SIGTERM', function () {
---
+++ src/send_mail.js
@@ -0,0 +1,32 @@
+﻿var nodemailer = require('nodemailer');
+
+function SendMail(send_to, mail_subject, mail_body, callback) {
+    var transporter = nodemailer.createTransport({
+        service: 'outlook',
+        auth: {
+            user: 'kiransj@outlook.com',
+            pass: '1by04CS027'
+        }
+    });
+
+    var mailOptions = {
+        from: 'Kiran S J ✔ <kiransj@outlook.com>', // sender address
+        to: send_to, 
+        subject: mail_subject, // Subject line
+        html: mail_body, // plaintext body
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+            callback(true);
+        }else{
+            console.log('Message sent: ' + info.response);
+            callback(false);
+        }
+    });
+}
+
+module.exports = {
+    SendMail: SendMail
+}
